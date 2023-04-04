95465fab66cedc588967f49230869bd6a1796daf
Wellery <wellerychaves@outlook.com>
2023-04-04 20:07:34 -0300

    feat(FreightAdmin): created a function to show only the order id related with freight

--- freight/admin.py
+++ freight/admin.py
@@ -5,4 +5,17 @@ from .models import Freight
 
 @admin.register(Freight)
 class FreightAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "id",
+        "get_order_id",
+        "carrier",
+        "delivery_time",
+        "delivery_cost",
+        "external_freight_id",
+    )
+
+    # get only order id
+    def get_order_id(self, object):
+        return object.order.id
+
+    get_order_id.short_description = "Order ID"
